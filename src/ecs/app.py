88e8cf785b07a2ea51bed0eaf0af4569f669d619
N0916167 <N0916167@my.ntu.ac.uk>
2023-12-11 16:38:57 +0000

    flask app running

--- src/ecs/app.py
+++ src/ecs/app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, session, jsonify
+from flask import Flask, render_template, session, jsonify, request
 from flask_session import Session
 import boto3
 import os
 
-############################################################
 # init app
 app = Flask(__name__)
 
-############################################################
 # Get environment variables
 dynamodb_session_table = os.environ.get('DYNAMODB_TABLE')
 fridge_mgr_lambda = os.environ.get('FRIDGE_MGR_ARN')
@@ -16,14 +14,12 @@ users_mgr_lambda = os.environ.get('USERS_MGR_ARN')
 health_report_mgr_lambda = os.environ.get('HEALTH_REPORT_MGR_ARN')
 token_mgr_lambda = os.environ.get('TOKEN_MGR_ARN')
 
-############################################################
 # config
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_DYNAMODB'] = boto3.resource('dynamodb', region_name='eu-west-1')
 app.config['SESSION_DYNAMODB_TABLE'] = dynamodb_session_table
 app.config['SESSION_PERMANENT'] = False
 
-############################################################
 # create lambda clients
 fridge_mgr_client = boto3.client('lambda', region_name='eu-west-1')
 order_mgr_client = boto3.client('lambda', region_name='eu-west-1')
@@ -31,11 +27,10 @@ users_mgr_client = boto3.client('lambda', region_name='eu-west-1')
 health_report_mgr_client = boto3.client('lambda', region_name='eu-west-1')
 token_mgr_client = boto3.client('lambda', region_name='eu-west-1')
 
-############################################################
 # init session
 Session(app)
 
-############################################################
+
 # session
 @app.route('/logout/')
 def logout():
@@ -44,19 +39,45 @@ def logout():
     print(session.get('username'))
     return jsonify({'status': '200'})
 
-############################################################
+
 # pages
-@app.route('/')
+@app.route('/',  methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    # if the button called home has been pressed, immediately return the home page
+    if request.form.get('home'):
+        print('home button pressed')
+        return render_template('home.html')
+    if 'username' in session: # whatever the auth is for if they are logged in or not
+        return render_template('home.html')
+    else:
+        if request.method == 'POST':
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            # however the call to the lamba function is done, I think it's something like this
+            response = token_mgr_client.invoke(
+                FunctionName=token_mgr_lambda,
+                InvocationType='RequestResponse',
+                Payload='{"username": "' + username + '", "password": "' + password + '"}'
+            )
+
+            response_payload = response['Payload'].read()
+            response_payload = response_payload.decode('utf-8')
+            response_payload = eval(response_payload)
+
+            if response_payload['status'] == '200':
+                session['username'] = username
+                return render_template('home.html')
+            else:
+                return render_template('login.html', error=response_payload['message'])
+        return render_template('login.html')
 
 
-@app.route('/other-page')
-def other_page():
-    return render_template('other-page.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 
-############################################################
 # run
 if __name__ == '__main__':
     app.run(debug=True)
