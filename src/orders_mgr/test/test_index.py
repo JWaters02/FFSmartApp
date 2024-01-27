38ca843a1ac7f8072d438778ba78c9943a046db1
JHale <n0920963@my.ntu.ac.uk>
2024-01-27 15:42:29 +0000

    added jacks test for orders_mgr

--- src/orders_mgr/test/test_index.py
+++ src/orders_mgr/test/test_index.py
@@ -1,9 +1,16 @@
 import unittest
 import time
+import secrets
 from unittest.mock import patch, MagicMock
 from src.index import handler
 from src.delete import delete_order, ClientError
 from src.post import create_order, NotFoundException
+from src.get import get_all_orders, get_order
+from src.custom_exceptions import BadRequestException
+from src.utils import (generate_order_id, is_order_id_valid, get_expired_item_quantity_fridge, get_item_quantity_fridge,
+                       get_item_quantity_orders, get_total_item_quantity)
+
+
 class TestDynamoDBHandler(unittest.TestCase):
     @patch('boto3.resource')
     def test_handler_function(self, mock_boto3_resource):  # use highly descriptive function names, not what like this
@@ -203,6 +210,211 @@ class TestCreateOrderFunction(unittest.TestCase):
                                     self.expired_items, self.table_name)
 
         self.assertEqual(response['statusCode'], 500)
+
+
+class TestGetAllOrdersFunction(unittest.TestCase):
+    def test_normal_parameters(self):
+        event = {'body': {'restaurant_id': 'example_restaurant'}}
+        self.table = MagicMock()
+
+        response = get_all_orders(event, self.table)
+
+        self.assertEqual(response['statusCode'], 200)
+
+    def test_event_missing_body(self):
+        event = {'example': 'example'}
+        self.table = MagicMock()
+
+        with self.assertRaises(BadRequestException) as context:
+            get_all_orders(event, self.table)
+        self.assertEqual(str(context.exception), 'Bad request restaurant_id not found in body.')
+
+    def test_event_missing_restaurant_id(self):
+        event = {'body': {'example': 'example'}}
+        self.table = MagicMock()
+
+        with self.assertRaises(BadRequestException) as context:
+            get_all_orders(event, self.table)
+        self.assertEqual(str(context.exception), 'Bad request restaurant_id not found in body.')
+
+    def test_key_error(self):
+        event = {'body': {'restaurant_id': 'example_restaurant'}}
+        self.table = MagicMock()
+        self.table.query.side_effect = KeyError()
+
+        response = get_all_orders(event, self.table)
+
+        self.assertEqual(response['statusCode'], 404)
+
+    def test_client_error(self):
+        event = {'body': {'restaurant_id': 'example_restaurant'}}
+        self.table = MagicMock()
+        self.table.query.side_effect = ClientError(error_response={'Error': {'Code': 'TestException'}},
+                                                   operation_name='query')
+
+        response = get_all_orders(event, self.table)
+
+        self.assertEqual(response['statusCode'], 500)
+
+
+class TestGetOrderFunction(unittest.TestCase):
+    def test_normal_parameters(self):
+        event = {'body': {'restaurant_id': 'example_restaurant', 'order_id': 'example_id'}}
+        self.table = MagicMock()
+
+        response = get_order(event, self.table)
+
+        self.assertEqual(response['statusCode'], 200)
+
+    def test_event_missing_body(self):
+        event = {'example': 'example'}
+        self.table = MagicMock()
+
+        with self.assertRaises(BadRequestException) as context:
+            get_order(event, self.table)
+        self.assertEqual(str(context.exception), 'Bad request restaurant_id not found in body.')
+
+    def test_event_missing_restaurant_id(self):
+        event = {'body': {'order_id': 'example_id'}}
+        self.table = MagicMock()
+
+        with self.assertRaises(BadRequestException) as context:
+            get_order(event, self.table)
+        self.assertEqual(str(context.exception), 'Bad request restaurant_id not found in body.')
+
+    def test_event_missing_order_id(self):
+        event = {'body': {'restaurant_id': 'example_restaurant'}}
+        self.table = MagicMock()
+
+        with self.assertRaises(BadRequestException) as context:
+            get_order(event, self.table)
+        self.assertEqual(str(context.exception), 'Bad request order_id not found in body.')
+
+    def test_key_error(self):
+        event = {'body': {'restaurant_id': 'example_restaurant', 'order_id': 'example_id'}}
+        self.table = MagicMock()
+        self.table.query.side_effect = KeyError()
+
+        response = get_order(event, self.table)
+
+        self.assertEqual(response['statusCode'], 404)
+
+    def test_client_error(self):
+        event = {'body': {'restaurant_id': 'example_restaurant', 'order_id': 'example_id'}}
+        self.table = MagicMock()
+        self.table.query.side_effect = ClientError(error_response={'Error': {'Code': 'TestException'}},
+                                                   operation_name='query')
+
+        response = get_order(event, self.table)
+
+        self.assertEqual(response['statusCode'], 500)
+
+
+class TestGenerateOrderIdFunction(unittest.TestCase):
+    @patch('src.utils.secrets')
+    def test_normal_parameters(self, mock_secrets):
+        self.table = MagicMock()
+        restaurant_id = 'example_restaurant'
+        mock_secrets.randbits.return_value = 1234567890123456
+
+        response = generate_order_id(self.table, restaurant_id)
+
+        self.assertEqual(response, '1234567890123456')
+
+    def test_missing_restaurant_id(self):
+        self.table = MagicMock()
+        restaurant_id = 'example_restaurant'
+        self.table.get_item.return_value = {'example': 'example'}
+
+        with self.assertRaises(NotFoundException) as context:
+            generate_order_id(self.table, restaurant_id)
+        self.assertEqual(str(context.exception), 'Restaurant does not exist.')
+
+
+class TestIsOrderIdValidFunction(unittest.TestCase):
+    def test_id_valid(self):
+        order_id = 1234567890123456
+        self.item = MagicMock()
+
+        response = is_order_id_valid(order_id, self.item)
+
+        self.assertEqual(response, True)
+
+    def test_id_invalid(self):
+        order_id = 1234567890123456
+        self.item = {'orders': [{'id': 1234567890123456}]}
+
+        response = is_order_id_valid(order_id, self.item)
+
+        self.assertEqual(response, False)
+
+
+class TestGetExpiredItemQuantityFridgeFunction(unittest.TestCase):
+    def test_normal_parameters(self):
+        self.fridge_item = {'item_list': [{'expiry_date': time.time()-999999, 'current_quantity': 10}]}
+
+        response = get_expired_item_quantity_fridge(self.fridge_item)
+
+        self.assertEqual(response, 10)
+
+    def test_zero_expired_item_quantity(self):
+        self.fridge_item = {'item_list': [{'expiry_date': time.time(), 'current_quantity': 10}]}
+
+        response = get_expired_item_quantity_fridge(self.fridge_item)
+
+        self.assertEqual(response, 0)
+
+
+class TestGetItemQuantityFridgeFunction(unittest.TestCase):
+    def test_normal_parameters(self):
+        self.fridge_item = {'item_list': [{'expiry_date': time.time(), 'current_quantity': 10}]}
+
+        response = get_item_quantity_fridge(self.fridge_item)
+
+        self.assertEqual(response, 10)
+
+    def test_zero_item_quantity(self):
+        self.fridge_item = {'item_list': [{'expiry_date': time.time() - 999999, 'current_quantity': 10}]}
+
+        response = get_item_quantity_fridge(self.fridge_item)
+
+        self.assertEqual(response, 0)
+
+
+class TestGetItemQuantityOrdersFunction(unittest.TestCase):
+    def test_normal_parameters(self):
+        order_items = [{'item_name': 'example_name', 'quantity': 10}]
+        item_name = 'example_name'
+
+        response = get_item_quantity_orders(order_items, item_name)
+
+        self.assertEqual(response, 10)
+
+    def test_no_matching_item_name(self):
+        order_items = [{'item_name': 'another_example_name', 'quantity': 10}]
+        item_name = 'example_name'
+
+        response = get_item_quantity_orders(order_items, item_name)
+
+        self.assertEqual(response, 0)
+
+
+class TestGetTotalItemQuantityFunction(unittest.TestCase):  # depends on previous two functions
+    def test_normal_parameters(self):
+        fridge_item = {'item_list': [{'expiry_date': time.time(), 'current_quantity': 5}], 'item_name': 'example_name'}
+        orders = [{'items': [{'item_name': 'example_name', 'quantity': 10}]}]
+
+        response = get_total_item_quantity(fridge_item, orders)
+
+        self.assertEqual(response, 15)
+
+    def test_only_fridge_item(self):
+        fridge_item = {'item_list': [{'expiry_date': time.time(), 'current_quantity': 5}], 'item_name': 'example_name'}
+        orders = [{'items': [{'item_name': 'another_example_name', 'quantity': 10}]}]
+
+        response = get_total_item_quantity(fridge_item, orders)
+
+        self.assertEqual(response, 5)
 
 
 if __name__ == '__main__':
