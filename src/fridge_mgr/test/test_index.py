92b6caea2ed86e275a422f40d202297cc46fe305
Sam Jiji <N0933896@my.ntu.ac.uk>
2024-02-06 13:50:00 +0000

    Updated test_index.py

--- src/fridge_mgr/test/test_index.py
+++ src/fridge_mgr/test/test_index.py
@@ -1,8 +1,8 @@
 import json
 import unittest
 from unittest.mock import patch, MagicMock, ANY
-from fridge_mgr.src.inventory_utils import modify_door_state, generate_response, delete_zero_quantity_items, update_item_quantity, add_new_item, add_delivery_item
-from fridge_mgr.src.inventory_utils import handler
+from .src.inventory_utils import modify_door_state, generate_response, delete_zero_quantity_items, update_item_quantity, add_new_item, add_delivery_item
+from .src.inventory_utils import handler
 class TestDynamoDBHandler(unittest.TestCase):
 
     @patch('boto3.resource')
