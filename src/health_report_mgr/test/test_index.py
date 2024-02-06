31e168c3db07a2ba61e400c8ca7d6ff62bd833bb
Sam Jiji <N0933896@my.ntu.ac.uk>
2024-02-06 16:50:56 +0000

    Updated test_index.py

--- src/health_report_mgr/test/test_index.py
+++ src/health_report_mgr/test/test_index.py
@@ -10,7 +10,7 @@ from src.health_report_mgr.src.utils import get_health_and_safety_email, get_fil
 class TestDynamoDBFunctions(unittest.TestCase):
     # Test the functions related to the DyanmoDB operations
 
-    @patch('src.utils.boto3')
+    @patch('src.health_report_mgr.src.utils.boto3')
     # Tests the get_health_and_safety_email function to ensure it correctly retrieves a health and safety email address from a DynamoDB table when it exists
     def test_get_health_and_safety_email_found(self, mock_boto3):
         #A mock DynamoDB table is created and configured to return a response containing an email when the get_item method is called
@@ -23,7 +23,7 @@ class TestDynamoDBFunctions(unittest.TestCase):
         # The assertion ensures that the returned email matches the mocked email set up.
         self.assertEqual(email, 'test@example.com')
 
-    @patch('src.utils.boto3')
+    @patch('src.health_report_mgr.src.utils.boto3')
     #Tests the get_health_and_safety_email function to ensure it returns nothing when the email address is not found in the DynamoDB table.
     def test_get_health_and_safety_email_not_found(self, mock_boto3):
         mock_table = Mock()
@@ -34,7 +34,7 @@ class TestDynamoDBFunctions(unittest.TestCase):
         # Assertion to verify that return is none which shows that no email was found.
         self.assertIsNone(email)
 
-    @patch('src.utils.boto3')
+    @patch('src.health_report_mgr.src.utils.boto3')
     #Tests the get_filtered_items function to ensure it can retrieve and filter items from a DynamoDB table based on provided date criteria.
     def test_get_filtered_items(self, mock_boto3):
         mock_table = Mock()
@@ -69,8 +69,8 @@ class TestDynamoDBFunctions(unittest.TestCase):
 
 class TestSendEmailWithAttachmentFunction (unittest.TestCase):
     # test the function works when given the expected parameters
-    @patch('src.utils.boto3.client')
-    @patch('src.utils.create_csv_content')
+    @patch('src.health_report_mgr.src.utils.boto3.client')
+    @patch('src.health_report_mgr.src.utils.create_csv_content')
     def test_normal_parameters(self, mock_create_csv_content, mock_boto3_client):
         mock_ses_client = MagicMock()
         mock_boto3_client.return_value = mock_ses_client
