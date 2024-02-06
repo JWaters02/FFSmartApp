9b04a6c16619616a40d800d294682aac263d1358
Sam Jiji <N0933896@my.ntu.ac.uk>
2024-02-06 14:33:59 +0000

    Updated test_index.py

--- src/health_report_mgr/test/test_index.py
+++ src/health_report_mgr/test/test_index.py
@@ -1,11 +1,11 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from src.index import handler
+from ..src.index import handler
 import json
 from boto3.dynamodb.conditions import Key
 import unittest
 from unittest.mock import Mock, patch
-from src.utils import get_health_and_safety_email, get_filtered_items, send_email_with_attachment
+from ..src.utils import get_health_and_safety_email, get_filtered_items, send_email_with_attachment
 
 
 
