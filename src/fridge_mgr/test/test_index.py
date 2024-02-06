5bbfbd24511a89f34a40429ebdae976eeb012f3b
Sam Jiji <N0933896@my.ntu.ac.uk>
2024-02-06 14:13:13 +0000

    Updated test_index.py

--- src/fridge_mgr/test/test_index.py
+++ src/fridge_mgr/test/test_index.py
@@ -1,6 +1,6 @@
 import json
 import unittest
-from unittest.mock import patch, MagicMock, ANY
+from unittest.mock import patch, MagicMock, ANY, Mock
 from ..src.inventory_utils import modify_door_state, generate_response, delete_zero_quantity_items, update_item_quantity, add_new_item, add_delivery_item
 from ..src.index import handler
 class TestDynamoDBHandler(unittest.TestCase):
